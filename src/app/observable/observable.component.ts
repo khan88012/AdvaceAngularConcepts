c48703fe1bac4ef8c3a4d5ccfa86dd81d71ecfd0
Md Asif Khan <asifkhan88012@gmail.com>
2023-09-04 15:15:42 +0530

    on an interval of 1 second we are pushing an integer into array and when clicking on getdata we can console it to see the content of the array

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observable.component.css']
 })
 export class ObservableComponent implements OnInit {
-
+  count= 0;
+  data : number[] =[];
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() =>
+    {
+      this.count++;
+      this.data.push(this.count);
+    },  1000);
   }
-
+  getdata(){
+    console.log(this.data);
+  }
 }
